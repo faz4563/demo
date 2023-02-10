bb8009b23725e8f2bf8df28a0a012ccf447397cf
mohamedfazil-at-029922603927 <mohamedfazil@neverskip.com>
2023-02-10 21:55:54 +0530

    working

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -275,70 +275,30 @@ class _LandingPageState extends State<LandingPage> {
                                           style: TextStyle(
                                               fontSize: 12,
                                               color: Colors.white)))
-                                  // Row(
-                                  //   children: [
-                                  //     WindowStyleDropdownMenu(
-                                  //         buttonTitle: "IT SOLUTIONS",
-                                  //         dropdownBackgroundColor: Colors.white,
-                                  //         dropdownItems: const [
-                                  //           ListTile(
-                                  //             title: Text(
-                                  //               "CYBER SECURITY",
-                                  //               style: TextStyle(
-                                  //                   color: Colors.blue),
-                                  //             ),
-                                  //           ),
-                                  //           ListTile(
-                                  //             title: Text("DATA CENTER"),
-                                  //           ),
-                                  //           ListTile(
-                                  //             title: Text("COLLABORATION"),
-                                  //           ),
-                                  //           ListTile(
-                                  //             title:
-                                  //                 Text("ENTERPRISE NETWORKS"),
-                                  //           ),
-                                  //           ListTile(
-                                  //             title: Text("CLOUD MIGRATION"),
-                                  //           ),
-                                  //           ListTile(
-                                  //             title: Text(
-                                  //                 "BACKUP AND DISASTER RECOVERY"),
-                                  //           ),
-                                  //         ]),
-                                  //     const Icon(Icons.keyboard_arrow_down)
-                                  //   ],
-                                  // ),
                                 ],
                               ),
                             )
-                            // InkWell(
-                            //   onHover: (value) {
-                            //     showDialog(
-                            //         context: context,
-                            //         builder: (context) => AlertDialog(
-                            //               content: Container(
-                            //                 child: Column(
-                            //                   children: const [
-                            //                     Text("CYBER SECURITY"),
-                            //                     Text("DATA CENTER"),
-                            //                     Text("COLLABORATION"),
-                            //                     Text("ENTERPRISE NETWORKS"),
-                            //                     Text("CLOUD MIGRATION"),
-                            //                     Text(
-                            //                         "BACKUP AND DISASTER RECOVERY"),
-                            //                   ],
-                            //                 ),
-                            //               ),
-                            //             ));
-                            //   },
-                            //   child: Text("IT SOLUTIONS"),
-                            // )
                           ],
+                        ),
+                      ),
+                      const Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Professional IT Services",
+                            style: TextStyle(
+                                fontSize: 62,
+                                color: Colors.white,
+                                // fontFamily: "Times New Roman",
+                                fontWeight: FontWeight.bold),
+                          ),
                         ),
                       )
                     ]),
-              )
+              ),
+              const SizedBox()
             ],
           ),
         ),
