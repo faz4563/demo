b2c0360b1691b0a72303348f3d20be209485074c
mohamedfazil-at-029922603927 <mohamedfazil@neverskip.com>
2023-02-21 13:06:29 +0530

    dir seperation

--- lib/screens/content.dart
+++ lib/screens/content.dart
@@ -1,4 +1,4 @@
-import 'package:demo/screens/industriesWeServe.dart';
+import 'package:demo/screens/INDUSTRIES%20WE%20SERVE/industriesWeServe.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
@@ -197,12 +197,7 @@ Our team of technicians, engineers and visionaries has guided organizations as t
       height: 1.5,
     ),
   );
-  static Text industriesWeServe = const Text("Industries We Serve : -",
-      style: TextStyle(
-          letterSpacing: 1,
-          fontSize: 18,
-          color: Colors.white,
-          fontWeight: FontWeight.w700));
+
   static Text isYourTech = const Text(
     '''
 Is your technology supporting your organization or standing in the way of growth? A complete assessment gives you an end-to-end view of your infrastructure, storage and networks. Our workshops include security strategy, workload education and infrastructure strategy.
@@ -215,71 +210,13 @@ We’ll help you''',
         fontSize: 14,
         fontWeight: FontWeight.w400),
   );
-  static String weAreNotJust =
-      '''We are not just another IT solutions provider capitalizing on the big data boom. We have a cause. Our goal is to help SMBs unlock the potential of advanced IT technology and compete with larger corporations and organizations. Our knowledge of SMBs and multi-disciplinary expertise have enabled us to offer unparalleled IT Solutions to businesses from various industries. Currently, we serve more than six industries that play a crucial role in nation-building and the global market.''';
 
-  static Text oilIndustries = const Text("Oil Industries : -",
-      style: TextStyle(
-          letterSpacing: 1,
-          fontSize: 18,
-          color: Colors.white,
-          fontWeight: FontWeight.w700));
-
-  static String oilGasIndustry =
-      '''The oil and gas industry is heavily dependent on data management and analysis. Our solutions help companies in the oil industry to optimize their operations, reduce downtime, and increase their overall profitability.''';
-
-  static Text healthCare = const Text(
-      "Healthcare & Other Service-Based Sectors : -",
-      style: TextStyle(
-          letterSpacing: 1,
-          fontSize: 18,
-          color: Colors.white,
-          fontWeight: FontWeight.w700));
-
-  static String itSolutions =
-      '''Our IT solutions help healthcare providers and service-based companies provide better care to their patients/clients, reduce administrative burdens, and optimize workflows.''';
-
-  static Text financial = const Text("Financial Consulting : -",
-      style: TextStyle(
-          letterSpacing: 1,
-          fontSize: 18,
-          color: Colors.white,
-          fontWeight: FontWeight.w700));
-
-  static String weOfferFinancial =
-      '''We offer financial management systems, cybersecurity, data analytics, and cloud computing solutions to financial consulting companies. Our solutions help these companies to improve their financial reporting, reduce fraud, and manage risk more effectively.''';
-
-  static Text education = const Text("Education : -",
-      style: TextStyle(
-          letterSpacing: 1,
-          fontSize: 18,
-          color: Colors.white,
-          fontWeight: FontWeight.w700));
-
-  static String weDevelop =
-      '''We develop integrated IT solutions for educational institutions, helping them improve student outcomes, reduce costs, and enhance overall effectiveness.''';
-
-  static Text retail = const Text("Retail Consulting Services : -",
-      style: TextStyle(
-          letterSpacing: 1,
-          fontSize: 18,
-          color: Colors.white,
-          fontWeight: FontWeight.w700));
-  static String companies =
-      '''Companies offering retail consulting services leverage our IT solutions to manage their operations, data, and customer interactions. Our solutions help retail consulting companies optimize their supply chain, improve customer service, and increase their overall profitability.''';
-
-  static Text transportation = const Text(
-    "Transportation, Logistics & Distribution : -",
-  );
   static Text whoWeAre = const Text("Who We Are: -",
       style: TextStyle(
           letterSpacing: 1,
           fontSize: 18,
           color: Colors.white,
           fontWeight: FontWeight.w700));
-
-  static String ourteamSpecializes =
-      ''' Our team specializes in developing and implementing IT solutions that help companies manage large amounts of data and optimize their logistics networks. With our solutions, companies can improve their logistics efficiency, reduce costs, and increase customer satisfaction.''';
 
   static String basedIn =
       "Based in Calgary, Canada, Avancer helps small and medium businesses navigate the complexity of IT infrastructure by providing enterprise-grade professional IT solutions.";
